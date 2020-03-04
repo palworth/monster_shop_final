18119ea17340e61cc3b3348bd71088b17c7e60aa
Pierce Alworth <pbalworth@gmail.com>
2020-03-03 19:06:13 -0700

    Update discount range model method

--- app/models/discount.rb
+++ app/models/discount.rb
@@ -7,6 +7,6 @@ class Discount < ApplicationRecord
                         :highest_amount
 
   def discount_range
-    "#{lowest_amount} - #{highest_amount} items"
+    "#{lowest_amount + 1} - #{highest_amount - 1} items"
   end
 end
